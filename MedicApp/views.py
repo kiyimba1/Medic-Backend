ae3545b27c48149b371342e3fcdde4c1e75af8c8
kiyimba1 <edwineddie54@gmail.com>
2021-04-11 23:14:07 +0300

    --- Auto-Git Commit ---

--- MedicApp/views.py
+++ MedicApp/views.py
@@ -1,3 +1,9 @@
+from MedicApp.serializes import CompanySerializer
+from MedicApp.models import Company
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer

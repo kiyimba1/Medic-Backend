7f136cf3cd8b41ffe060fc5aed5878f0e7be4754
kiyimba1 <edwineddie54@gmail.com>
2021-04-11 14:15:00 +0300

    Added models to Admin site

--- MedicApp/admin.py
+++ MedicApp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Company,CompanyAccount,CompanyBank, EmployeeBank, Medicine, MedicalDetails, Employee, EmployeeSalary, Customer, CustomerRequest, Bill, BillDetails
+
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Medicine)
+admin.site.register(MedicalDetails)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Bill)
+admin.site.register(EmployeeSalary)
+admin.site.register(BillDetails)
+admin.site.register(CustomerRequest)
+admin.site.register(CompanyAccount)
+admin.site.register(CompanyBank)
+admin.site.register(EmployeeBank)
+
